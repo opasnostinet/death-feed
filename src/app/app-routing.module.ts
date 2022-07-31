035df21da330e7ab94ff77aa5128e23b20973a13
opasnostinet <veselovalova@icloud.com>
2022-07-31 18:24:53 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
+import { ChatsComponent } from './pages/chats/chats.component';
+import { FallbackComponent } from './pages/fallback/fallback.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: '', component: HomeComponent},
+  {path: 'feed', component: FeedComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'profile/:userId', component: ProfileComponent},
+  {path: 'chats', component: ChatsComponent},
+  {path: 'chat/:userId', component: ChatComponent},
+  {path: '**', component: FallbackComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ChatComponent } from './pages/chat/chat.component';
+import { ChatsComponent } from './pages/chats/chats.component';
+import { FallbackComponent } from './pages/fallback/fallback.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    FeedComponent,
+    ProfileComponent,
+    ChatComponent,
+    ChatsComponent,
+    FallbackComponent,
   ],
   imports: [
     BrowserModule,
